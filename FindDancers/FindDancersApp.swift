e25f836b34e9153774473583dcf832707421c9c4
JakobBlue <jakobtobiasweitzel@icloud.com>
2026-08-03 14:23:49 +0200

    Initial Commit

---
+++ FindDancers/FindDancersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindDancersApp.swift
+//  FindDancers
+//
+//  Created by Jakob Tobias Weitzel on 03.08.26.
+//
+
+import SwiftUI
+
+@main
+struct FindDancersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
